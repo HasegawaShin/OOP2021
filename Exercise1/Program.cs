9f28226e8e46c3f1a218a473bb2355cfc23db33a
HasegawaShin <32094@ojs.ac.jp>
2021-05-28 15:00:07 +0900

    P79 問題2.1

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+            List<Song> songs = new List<Song>();
+
+            songs.Add(new Song("Lemon", "米津玄師", 240));
+            songs.Add(new Song("Loser", "米津玄師", 230));
+
+        }
+
+        private static void PrintSongs(Song[] songs) {
+            foreach (var i in songs) {
+                Console.WriteLine("曲名：{0} アーティスト：{1} 演奏時間：{2}:{3:00}", i.Title1, i.ArtistName1, (i.Length1 / 60), (i.Length1 % 60));
+            }
         }
     }
 }
---
+++ Exercise1/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Song {
+        private string Title; // 歌のタイトル
+        private string ArtistName; // アーティスト
+        private int Length; // 演奏時間 (s)
+
+        public string Title1 { get => Title; set => Title = value; }
+        public string ArtistName1 { get => ArtistName; set => ArtistName = value; }
+        public int Length1 { get => Length; set => Length = value; }
+
+        public Song(string Title, string ArtistName, int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+
+    }
+}
