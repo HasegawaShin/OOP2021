f6fca0bf8839cb6738195efc64f2b6ebf44702f2
HasegawaShin <32094@ojs.ac.jp>
2021-06-01 11:24:54 +0900

    P79 問題2.3 完成

---
+++ Exercise3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+
+            var sales = new SalesCounter("Sales.csv");
+
+            var amoountPerCategory = sales.GetPerCategorySales();
+
+            foreach (var obj in amoountPerCategory) {
+                Console.WriteLine("{0} {1:#,#} 円", obj.Key, obj.Value);
+            }
+        }
+    }
+}
