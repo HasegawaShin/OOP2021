e095731cf3680c1d0d8414b54677098f65c53d83
HasegawaShin <32094@ojs.ac.jp>
2021-06-11 15:25:20 +0900

    P134 問題

--- Chapter04/Exercise/Exercise2/Program.cs
+++ Chapter04/Exercise/Exercise2/Program.cs
@@ -9,27 +9,30 @@ namespace Exercise2 {
     class Program {
         static void Main(string[] args) {
             // 4.2.1
-            var YearMonths = new YearMonth[] {
+            var ymCollection = new YearMonth[] {
                 new YearMonth(2101, 2),
                 new YearMonth(2111, 8),
                 new YearMonth(2112, 10),
-                new YearMonth(2021, 5),
+                new YearMonth(2021, 12),
                 new YearMonth(2323, 6),
             };
 
             // 4.2.2
-            foreach (var ym in YearMonths) {
+            foreach (var ym in ymCollection) {
                 Console.WriteLine(ym);
             }
 
             // 4.2.4
-            if (First21Century(YearMonths) == null) {
-                Console.WriteLine("21世紀のデータはありません。");
-            } else {
-                Console.WriteLine(First21Century(YearMonths).Year);
-            }
+            var yearmonth = First21Century(ymCollection);
+            var s = yearmonth != null ? yearmonth.Year.ToString() : "21世紀のデータはありません";
 
+            // 4.2.5
 
+            // ToArray():配列に格納して　　ToList():リストに格納して
+            var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach (var ym in array) {
+                Console.WriteLine(ym);
+            } 
 
         }
 
