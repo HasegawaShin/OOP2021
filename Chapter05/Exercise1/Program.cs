fb723818f8966df8617dfb662ee1f4aca4974091
HasegawaShin <32094@ojs.ac.jp>
2021-06-21 13:25:12 +0900

    1期目試験

---
+++ Chapter05/Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            #region 問題5.1
+            // 5.1
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, ignoreCase : true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+
+            Console.WriteLine("-------------------");
+            #endregion
+
+            // 5.2 未完成
+            var str = int.TryParse(Console.ReadLine(), out int number);
+            Console.WriteLine(number);
+
+        }
+    }
+}
