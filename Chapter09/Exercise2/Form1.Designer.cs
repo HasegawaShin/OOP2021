7dc8a42740130790a93a1099ba952aff3ca2301f
心 長谷川 <32094@ojs.ac.jp>
2021-07-19 15:56:44 +0900

    夏休み前

---
+++ Chapter09/Exercise2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Exercise2 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btOpen = new System.Windows.Forms.Button();
+            this.btChangeFile = new System.Windows.Forms.Button();
+            this.btChange = new System.Windows.Forms.Button();
+            this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btOpen
+            // 
+            this.btOpen.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btOpen.Location = new System.Drawing.Point(38, 64);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(142, 55);
+            this.btOpen.TabIndex = 0;
+            this.btOpen.Text = "開く...";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // btChangeFile
+            // 
+            this.btChangeFile.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btChangeFile.Location = new System.Drawing.Point(38, 152);
+            this.btChangeFile.Name = "btChangeFile";
+            this.btChangeFile.Size = new System.Drawing.Size(142, 55);
+            this.btChangeFile.TabIndex = 1;
+            this.btChangeFile.Text = "変換先";
+            this.btChangeFile.UseVisualStyleBackColor = true;
+            this.btChangeFile.Click += new System.EventHandler(this.btChangeFile_Click);
+            // 
+            // btChange
+            // 
+            this.btChange.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btChange.Location = new System.Drawing.Point(38, 246);
+            this.btChange.Name = "btChange";
+            this.btChange.Size = new System.Drawing.Size(142, 52);
+            this.btChange.TabIndex = 2;
+            this.btChange.Text = "行番号追加";
+            this.btChange.UseVisualStyleBackColor = true;
+            this.btChange.Click += new System.EventHandler(this.btChange_Click);
+            // 
+            // ofdOpenFile
+            // 
+            this.ofdOpenFile.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 366);
+            this.Controls.Add(this.btChange);
+            this.Controls.Add(this.btChangeFile);
+            this.Controls.Add(this.btOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Button btChangeFile;
+        private System.Windows.Forms.Button btChange;
+        private System.Windows.Forms.OpenFileDialog ofdOpenFile;
+        private System.Windows.Forms.SaveFileDialog sfdSaveFile;
+    }
+}
+
