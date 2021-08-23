1955b288b9be5d286919c128c38bdfba3557fded
心 長谷川 <32094@ojs.ac.jp>
2021-08-23 15:53:39 +0900

    20210823べき乗

---
+++ Sample0823/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Exec_Click(object sender, EventArgs e) {
+            
+            // 整数に変換
+            int value = int.Parse(Value.Text);
+            int jyou = int.Parse(Jyou.Text);
+
+            double result = Math.Pow(value, jyou);
+
+            // for (int i = 0; i < jyou; i++) {
+            //     result = result * value;
+            // }
+
+            Result.Text = result.ToString();
+        }
+    }
+}
